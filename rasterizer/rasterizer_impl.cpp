fd22932329763dacd9b72746c47e0e56789d98ce
Ethan <3219682071@qq.com>
2024-09-10 22:20:56 -0400

    first upload

---
+++ rasterizer/rasterizer_impl.cpp
@@ -0,0 +1,101 @@
+#include <cstdint>
+#include "image.hpp"
+#include "loader.hpp"
+#include "rasterizer.hpp"
+
+// TODO
+void Rasterizer::DrawPixel(uint32_t x, uint32_t y, Triangle trig, AntiAliasConfig config, uint32_t spp, Image& image, Color color)
+{
+    if (config == AntiAliasConfig::NONE)            // if anti-aliasing is off
+    {
+
+    }
+    else if (config == AntiAliasConfig::SSAA)       // if anti-aliasing is on
+    {
+
+    }
+
+    // if the pixel is inside the triangle
+    image.Set(x, y, color);
+
+    return;
+}
+
+// TODO
+void Rasterizer::AddModel(MeshTransform transform, glm::mat4 rotation)
+{
+    /* model.push_back( model transformation constructed from translation, rotation and scale );*/
+    return;
+}
+
+// TODO
+void Rasterizer::SetView()
+{
+    const Camera& camera = this->loader.GetCamera();
+    glm::vec3 cameraPos = camera.pos;
+    glm::vec3 cameraLookAt = camera.lookAt;
+    glm::vec3 cameraUp = camera.up;
+
+    // TODO change this line to the correct view matrix
+    this->view = glm::mat4(1.);
+
+    return;
+}
+
+// TODO
+void Rasterizer::SetProjection()
+{
+    const Camera& camera = this->loader.GetCamera();
+
+    float nearClip = camera.nearClip;                   // near clipping distance, strictly positive
+    float farClip = camera.farClip;                     // far clipping distance, strictly positive
+    
+    float width = this->loader.GetWidth();
+    float height = this->loader.GetHeight();
+    
+    // TODO change this line to the correct projection matrix
+    this->projection = glm::mat4(1.);
+
+    return;
+}
+
+// TODO
+void Rasterizer::SetScreenSpace()
+{
+    float width = this->loader.GetWidth();
+    float height = this->loader.GetHeight();
+
+    // TODO change this line to the correct screenspace matrix
+    this->screenspace = glm::mat4(1.);
+
+    return;
+}
+
+// TODO
+glm::vec3 Rasterizer::BarycentricCoordinate(glm::vec2 pos, Triangle trig)
+{
+    return glm::vec3();
+}
+
+// TODO
+float Rasterizer::zBufferDefault = float();
+
+// TODO
+void Rasterizer::UpdateDepthAtPixel(uint32_t x, uint32_t y, Triangle original, Triangle transformed, ImageGrey& ZBuffer)
+{
+
+    float result;
+    ZBuffer.Set(x, y, result);
+
+    return;
+}
+
+// TODO
+void Rasterizer::ShadeAtPixel(uint32_t x, uint32_t y, Triangle original, Triangle transformed, Image& image)
+{
+
+    Color result;
+    image.Set(x, y, result);
+
+    return;
+}
