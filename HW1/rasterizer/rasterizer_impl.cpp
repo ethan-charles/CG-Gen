b2716ba5256d75dd242a84f1a0225b538cea47ac
Ethan <3219682071@qq.com>
2024-09-20 22:15:38 -0400

    HW1 final

--- HW1/rasterizer/rasterizer_impl.cpp
+++ HW1/rasterizer/rasterizer_impl.cpp
@@ -1,3 +1,4 @@
+// Yichen Lu nechy@umich.edu
 #include <cstdint>
 #include "image.hpp"
 #include "loader.hpp"
@@ -37,83 +38,75 @@ void Rasterizer::DrawPixel(uint32_t x, uint32_t y, Triangle trig, AntiAliasConfi
     if (config == AntiAliasConfig::NONE)            // if anti-aliasing is off
     {
         glm::vec3 pixel_center(x + 0.5f, y + 0.5f, 0.0f); 
-        if (IsPointInsideTriangle(pixel_center, trig.pos[0], trig.pos[1], trig.pos[2]))
+        if (IsPointInsideTriangle(pixel_center, trig.pos[0], trig.pos[1], trig.pos[2])) //if the pixel is inside the triangle
         {
-            image.Set(x, y, color);
+            //Write to the corresponding pixel
+            image.Set(x, y, color); 
         }
     }
     else if (config == AntiAliasConfig::SSAA)       // if anti-aliasing is on
     {
         uint32_t sample_count = 0;
-
-        // Loop over the samples per pixel (spp)
         for (uint32_t i = 0; i < spp; ++i)
         {
-        
+            //Generate sample points based on spp;
             float sample_x = static_cast<float>(x) + static_cast<float>(rand()) / RAND_MAX;
             float sample_y = static_cast<float>(y) + static_cast<float>(rand()) / RAND_MAX;
-            glm::vec3 sample_point(sample_x, sample_y, 0.0f);
+            glm::vec3 sample_point(sample_x, sample_y, 0.0f); 
 
-            
-            if (IsPointInsideTriangle(sample_point, trig.pos[0], trig.pos[1], trig.pos[2]))
+            if (IsPointInsideTriangle(sample_point, trig.pos[0], trig.pos[1], trig.pos[2])) //if the sample point is inside the triangle
             {
                 ++sample_count;
-                // if the pixel is inside the triangle
-                
             }
         }
 
-        
+        //Write to the pixel, with value based on count and spp;
         float coverage = static_cast<float>(sample_count) / static_cast<float>(spp);
         Color final_color = color * coverage;
-        image.Set(x, y, final_color);
+        image.Set(x, y, final_color); //
     }
-
-
     return;
 }
 
 // TODO
 void Rasterizer::AddModel(MeshTransform transform, glm::mat4 rotation)
 {
-    /* model.push_back( model transformation constructed from translation, rotation and scale );*/
+    
     glm::mat4 Mrot = rotation;
     glm::mat4 Mtrans = glm::translate(glm::mat4(1.0f), transform.translation);
     glm::mat4 Mscale = glm::scale(glm::mat4(1.0f), transform.scale);
 
     glm::mat4 Mmodel = Mrot * Mtrans * Mscale;
 
+    /* model.push_back( model transformation constructed from translation, rotation and scale );*/
     this->model.push_back(Mmodel);
-
     return;
 }
 
 // TODO
 void Rasterizer::SetView()
-{
+{  
+    //Get Camera from Loader;
     const Camera& camera = this->loader.GetCamera();
     glm::vec3 cameraPos = camera.pos;
     glm::vec3 cameraLookAt = camera.lookAt;
     glm::vec3 cameraUp = camera.up;
 
-    
-    // Compute the forward (g), right (f), and up (h) vectors
-    glm::vec3 g = glm::normalize(cameraLookAt - cameraPos);    // Forward vector
-    glm::vec3 f = glm::normalize(glm::cross(g, cameraUp));     // Right vector
-    glm::vec3 h = glm::cross(f, g);                            // Up vector (recomputed for orthogonality)
+    glm::vec3 g = glm::normalize(cameraLookAt - cameraPos); //Forward
+    glm::vec3 f = glm::normalize(glm::cross(g, cameraUp));  //Right
+    glm::vec3 h = glm::cross(f, g); //Up
 
-    // Construct the rotation matrix (Mrot)
+    //Create Mrot by orienting the camera towards negative z-axis;
     glm::mat4 Mrot(1.0f);
     Mrot[0][0] = f.x; Mrot[0][1] = h.x; Mrot[0][2] = -g.x; Mrot[0][3] = 0.0f;
     Mrot[1][0] = f.y; Mrot[1][1] = h.y; Mrot[1][2] = -g.y; Mrot[1][3] = 0.0f;
     Mrot[2][0] = f.z; Mrot[2][1] = h.z; Mrot[2][2] = -g.z; Mrot[2][3] = 0.0f;
     Mrot[3][0] = 0.0f; Mrot[3][1] = 0.0f; Mrot[3][2] = 0.0f; Mrot[3][3] = 1.0f;
 
-    // Construct the translation matrix (Mtrans)
+    //Create Mtrans by moving the camera to the origin;
     glm::mat4 Mtrans = glm::translate(glm::mat4(1.0f), -cameraPos);
 
-    // Combine rotation and translation to get the view matrix
-    // TODO change this line to the correct view matrix
+    //Combine Mrot and Mtrans to get Mview;
     this->view = glm::mat4(1.);
     this->view = Mrot * Mtrans;
     return;
@@ -122,65 +115,54 @@ void Rasterizer::SetView()
 // TODO
 void Rasterizer::SetProjection()
 {
+    //Get camera parameters from Loader;
     const Camera& camera = this->loader.GetCamera();
-
-    float nearClip = -camera.nearClip;                   // near clipping distance, strictly positive
-    float farClip = -camera.farClip;      
+    float nearClip = -camera.nearClip; //positive
+    float farClip = -camera.farClip; //positive     
     float camera_width = camera.width;
-    float camera_height = camera.height;               // far clipping distance, strictly positive
-    
+    float camera_height = camera.height;
+
     float width = this->loader.GetWidth();
     float height = this->loader.GetHeight();
 
-    // Step 15: Create Mpersp to squish the objects to conform to perspective
+    //Create Mpersp to squish the objects to conform to perspective
     glm::mat4 Mpersp = glm::mat4(0.0f);
-
     Mpersp[0][0] = nearClip;
     Mpersp[1][1] = nearClip;
-    Mpersp[2][2] = nearClip + farClip;
+    Mpersp[2][2] = nearClip + farClip; 
     Mpersp[2][3] = 1.0f;
     Mpersp[3][2] = - nearClip * farClip;
 
-    // Create the scaling matrix for orthographic projection
-    glm::mat4 M_ortho_scale = glm::mat4(1.0f);
-    M_ortho_scale[0][0] = 2.0f / camera_width;
-    M_ortho_scale[1][1] = 2.0f / camera_height;
-    M_ortho_scale[2][2] = 2.0f / (nearClip - farClip); // Negative to account for the z-axis direction
+    //Create Mortho to rescale and translate the objects to the canonical space
+    glm::mat4 Mortho_scale = glm::mat4(1.0f);
+    Mortho_scale[0][0] = 2.0f / camera_width;
+    Mortho_scale[1][1] = 2.0f / camera_height;
+    Mortho_scale[2][2] = 2.0f / (nearClip - farClip);
 
-    // Create the translation matrix for orthographic projection
-    glm::mat4 M_ortho_trans = glm::mat4(1.0f);
-    M_ortho_trans[3][2] = - (nearClip + farClip) / 2.0f;                        // Centers the z-axis
+    glm::mat4 Mortho_trans = glm::mat4(1.0f);
+    Mortho_trans[3][2] = - (nearClip + farClip) / 2.0f;
 
-    // Combine the scaling and translation to get the orthographic matrix
-    glm::mat4 M_ortho = M_ortho_scale * M_ortho_trans;
-
-    // Step 17: Combine Mpersp and Mortho to get Mprojection
-    this->projection = M_ortho * Mpersp;
+    this->projection = Mortho_scale * Mortho_trans * Mpersp;
 
     return;
 }
 
 // TODO
 void Rasterizer::SetScreenSpace()
-{
+{   
+    //Get camera parameters from loader
     float width = this->loader.GetWidth();
     float height = this->loader.GetHeight();
 
-    // TODO change this line to the correct screenspace matrix
-    // Initialize the screen space transformation matrix to identity
-    glm::mat4 screenspace = glm::mat4(1.0f);
+    //Create Mss based on the width and height of the canvas, and preserve the z value;
+    glm::mat4 Mss = glm::mat4(1.0f);
 
-    // Scale x and y by width/2 and height/2 respectively
-    screenspace[0][0] = width / 2.0f;   // Scale x
-    screenspace[1][1] = height / 2.0f; // Scale y and flip y-axis (if needed)
-    screenspace[2][2] = 1.0f;           // Preserve z
+    Mss[0][0] = width / 2.0f;
+    Mss[1][1] = height / 2.0f;
+    Mss[2][2] = 1.0f;
+    Mss[3][0] = width / 2.0f; Mss[3][1] = height / 2.0f; Mss[3][2] = 0.0f;
 
-    // Translate x and y by width/2 and height/2 respectively
-    screenspace[3][0] = width / 2.0f;   // Translate x
-    screenspace[3][1] = height / 2.0f;  // Translate y
-    screenspace[3][2] = 0.0f;           // No translation in z
-
-    this->screenspace = screenspace;
+    this->screenspace = Mss;
     
     return;
 }
@@ -192,114 +174,47 @@ glm::vec3 Rasterizer::BarycentricCoordinate(glm::vec2 pos, Triangle trig)
     glm::vec3 v2 = glm::vec3(trig.pos[1].x, trig.pos[1].y, 0);
     glm::vec3 v3 = glm::vec3(trig.pos[2].x, trig.pos[2].y, 0);
     
-    // 计算三角形的面积
-    float area_inv = 1.0f / ((v2.y - v3.y) * (v1.x - v3.x) + (v3.x - v2.x) * (v1.y - v3.y));
+    float alpha = ((v2.y - v3.y) * (pos.x - v3.x) + (v3.x - v2.x) * (pos.y - v3.y));
+    float beta = ((v3.y - v1.y) * (pos.x - v3.x) + (v1.x - v3.x) * (pos.y - v3.y));
 
-    // 计算重心坐标
-    float alpha = ((v2.y - v3.y) * (pos.x - v3.x) + (v3.x - v2.x) * (pos.y - v3.y)) * area_inv;
-    float beta = ((v3.y - v1.y) * (pos.x - v3.x) + (v1.x - v3.x) * (pos.y - v3.y)) * area_inv;
-    float gamma = 1.0f - alpha - beta;
+    float area = ((v2.y - v3.y) * (v1.x - v3.x) + (v3.x - v2.x) * (v1.y - v3.y));
+    float alpha_nor = alpha / area;
+    float beta_nor = beta / area;
 
-    // 返回重心坐标
-    return glm::vec3(alpha, beta, gamma);
+    return glm::vec3(alpha_nor, beta_nor, 1.0f - alpha_nor - beta_nor);
 }
 
 // TODO
-float Rasterizer::zBufferDefault = -1.1f;
+float Rasterizer::zBufferDefault = -1.0f;
 
 // TODO
 void Rasterizer::UpdateDepthAtPixel(uint32_t x, uint32_t y, Triangle original, Triangle transformed, ImageGrey& ZBuffer)
 {
     glm::vec2 pos(x+0.5f, y+0.5f);
-
-    // Extract triangle vertices in screen space
     glm::vec3 v0(transformed.pos[0].x, transformed.pos[0].y, 0.0f);
     glm::vec3 v1(transformed.pos[1].x, transformed.pos[1].y, 0.0f);
     glm::vec3 v2(transformed.pos[2].x, transformed.pos[2].y, 0.0f);
     glm::vec3 pos3(pos, 0.0f);
 
-    // Check if the point is inside the triangle using IsPointInsideTriangle
     if (IsPointInsideTriangle(pos3, v0, v1, v2)){
-        // Compute barycentric coordinates at this pixel
         glm::vec3 bary = BarycentricCoordinate(pos, transformed);
-        // Get depth values at the triangle vertices (after projection and perspective divide)
-        float result = glm::dot(bary, glm::vec3(original.pos[0].z, original.pos[1].z, original.pos[2].z));
-        // float currentDepth = ZBuffer.Get(x, y).value_or(Rasterizer::zBufferDefault);
-        std::optional<float> currentDepth = ZBuffer.Get(x, y);
-        if (result > currentDepth)
+
+        //Compute the depth of the pixel using barycentric coordinates of triangle
+        float depth = glm::dot(bary, glm::vec3(original.pos[0].z, original.pos[1].z, original.pos[2].z));
+
+        if (depth > ZBuffer.Get(x, y))
         {
-            ZBuffer.Set(x, y, result);
+            ZBuffer.Set(x, y, depth);
         }
     }
 
     return;
 }
 
-Color CalculateColor_BlinnPhong(glm::vec3 pos, glm::vec3 normal, glm::vec3 view_pos, std::vector<Light> lights, Color ambient, float specularExponent)
-{
-    Color result = Color(0.0f, 0.0f, 0.0f, 0.0f);
-    glm::vec3 lightDir;
-    glm::vec3 half;
-    float diffuse;
-    float specular;
-    Color ambientColor = ambient;
-    Color diffuseColor;
-    Color specularColor;
-    glm::vec3 view = glm::normalize(view_pos - pos);
-
-    float diffuse_decay;
-    float specular_decay;
-    result = ambientColor;
-
-    specularExponent = specularExponent;
-    for (auto& light : lights)
-    {
-        // Calculate the light direction
-        lightDir = glm::normalize(light.pos - pos);
-
-        // Calculate the half vector
-        half = glm::normalize(lightDir + view);
-
-        // Calculate the diffuse term
-        diffuse = glm::max(glm::dot(normal, lightDir), 0.0f);
-
-        // Calculate the specular term
-        specular = glm::pow(glm::max(glm::dot(normal, half), 0.0f), specularExponent);
-
-        // Calculate the light decay
-        diffuse_decay = light.intensity / (glm::length(light.pos - pos) * glm::length(light.pos - pos));
-
-        // Calculate the diffuse term
-        diffuseColor = diffuse_decay * light.color * diffuse ;
-
-        // Calculate the specular decay
-        specular_decay = light.intensity / (glm::length(light.pos - pos) * glm::length(light.pos - pos));
-
-        // Calculate the specular term
-        specularColor = specular_decay* light.color * specular;
-
-        // Calculate the final color
-        result = diffuseColor + specularColor + result;
-    }
-    return result;
-}
-
 // TODO
 void Rasterizer::ShadeAtPixel(uint32_t x, uint32_t y, Triangle original, Triangle transformed, Image& image)
 {
-
-    // Calculate the barycentric coordinates of the pixel
-    Color result;
-    float depth;
-    
-    std::vector<Light> lights = this->loader.GetLights();
-    Color ambient = this->loader.GetAmbientColor();
-    float specularExponent = this->loader.GetSpecularExponent();
-    glm::vec3 cam_pos = this->loader.GetCamera().pos;
-
     glm::vec2 pos(x+0.5f, y+0.5f);
-
-    // Extract triangle vertices in screen space
     glm::vec3 v0(transformed.pos[0].x, transformed.pos[0].y, 0.0f);
     glm::vec3 v1(transformed.pos[1].x, transformed.pos[1].y, 0.0f);
     glm::vec3 v2(transformed.pos[2].x, transformed.pos[2].y, 0.0f);
@@ -307,63 +222,34 @@ void Rasterizer::ShadeAtPixel(uint32_t x, uint32_t y, Triangle original, Triangl
 
     if (IsPointInsideTriangle(pos3, v0, v1, v2))
     {
-        glm::vec3 bary = BarycentricCoordinate(glm::vec2(x + 0.5, y + 0.5), transformed);           // Bug Fix: x + 0.5, y + 0.5, or the pixel will be at the top-left corner of the triangle
+        glm::vec3 bary = BarycentricCoordinate(pos, transformed);
 
-        // Calculate the original depth of the pixel
-        depth = glm::dot(bary, glm::vec3(original.pos[0].z, original.pos[1].z, original.pos[2].z));
+        //Compute the depth of the pixel using barycentric coordinates of triangle
+        float depth = glm::dot(bary, glm::vec3(original.pos[0].z, original.pos[1].z, original.pos[2].z));
 
         if (depth == this->ZBuffer.Get(x, y))
         {
-            // Calculate the normal of the pixel
+            glm::vec3 original_coords = bary.x * glm::vec3(original.pos[0]) + bary.y * glm::vec3(original.pos[1]) + bary.z * glm::vec3(original.pos[2]);
+            //Retrieve its normal using barycentric coordinates
             glm::vec3 normal = bary.x * glm::vec3(original.normal[0]) + bary.y * glm::vec3(original.normal[1]) + bary.z * glm::vec3(original.normal[2]);
             normal = glm::normalize(normal);
 
-            glm::vec3 original_coords = bary.x * glm::vec3(original.pos[0]) + bary.y * glm::vec3(original.pos[1]) + bary.z * glm::vec3(original.pos[2]);
+            std::vector<Light> lights = this->loader.GetLights();
+            Color ambientColor = this->loader.GetAmbientColor();
+            float specularExponent = this->loader.GetSpecularExponent();
+            glm::vec3 Camera = this->loader.GetCamera().pos;
 
-
-            Color result = Color(0.0f, 0.0f, 0.0f, 0.0f);
-            glm::vec3 lightDir;
-            glm::vec3 half;
-            float diffuse;
-            float specular;
-            Color ambientColor = ambient;
-            Color diffuseColor;
-            Color specularColor;
-            glm::vec3 view = glm::normalize(cam_pos - original_coords);
-
-            float diffuse_decay;
-            float specular_decay;
-            result = ambientColor;
-
-            specularExponent = specularExponent;
+            Color result = ambientColor;
             for (auto& light : lights)
             {
-                // Calculate the light direction
-                lightDir = glm::normalize(light.pos - original_coords);
+                glm::vec3 l = glm::normalize(light.pos - original_coords);
+                glm::vec3 h = glm::normalize(l + glm::normalize(Camera - original_coords));
+                float r = glm::length(light.pos - original_coords);
+                
+                Color L_d = light.color * (light.intensity / (std::pow(r, 2)) * std::max(glm::dot(l, normal), 0.0f));
+                Color L_s = light.color * (light.intensity / (r * r)) * glm::pow(glm::max(glm::dot(normal, h), 0.0f), specularExponent);
 
-                // Calculate the half vector
-                half = glm::normalize(lightDir + view);
-
-                // Calculate the diffuse term
-                diffuse = glm::max(glm::dot(normal, lightDir), 0.0f);
-
-                // Calculate the specular term
-                specular = glm::pow(glm::max(glm::dot(normal, half), 0.0f), specularExponent);
-
-                // Calculate the light decay
-                diffuse_decay = light.intensity / (glm::length(light.pos - original_coords) * glm::length(light.pos - original_coords));
-
-                // Calculate the diffuse term
-                diffuseColor = diffuse_decay * light.color * diffuse ;
-
-                // Calculate the specular decay
-                specular_decay = light.intensity / (glm::length(light.pos - original_coords) * glm::length(light.pos - original_coords));
-
-                // Calculate the specular term
-                specularColor = specular_decay* light.color * specular;
-
-                // Calculate the final color
-                result = diffuseColor + specularColor + result;
+                result = result + L_d + L_s;
             }
 
             image.Set(x, y, result);
